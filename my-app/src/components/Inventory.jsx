0bc1f61b21cf676bdca46c873168b8b9f1c0457f
Cihad Akkaya <akkayac84@gmail.com>
2023-01-02 20:49:52 -0500

    implemented material io.

--- my-app/src/components/Inventory.jsx
+++ my-app/src/components/Inventory.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState}from 'react';
 import './css/inventory.css';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Fab from '@mui/material/Fab';
 
 function Inventory () {
-  let list = [{name:'eggs'}, {name:'milk'}, ]
+  const [inventory, setInventory] = useState([{name:'eggs'}, {name:'milk'} ])
+
+  let handleSubmit = (event) => {
+    setInventory([...inventory, {name:event.target[1].value}])
+    event.preventDefault();
+  }
   return (
     <div className='Inventory'>
-      {list.map((item) =>
-      <li></li>)}
-      <span className='ingredient'>eggs</span>
-      <span className='ingredient'>milk</span>
+      <form onSubmit={handleSubmit}>
+        <Button variant="contained" >Contained</Button>
+        <TextField id="outlined-basic" label="Add Stock" variant="outlined" />
+      </form>
+
+      <FormGroup>
+        {inventory.map((item, index) =>
+          <div>
+            <FormControlLabel key={index} control={<Checkbox defaultChecked />} label={item.name} />
+            <Fab size='small' color="primary" aria-label="add">
+              <RemoveIcon />
+            </Fab>
+
+          </div>
+          )}
+      </FormGroup>
     </div>
 
   );
